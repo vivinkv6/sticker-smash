bdd53539e85dc11626e25fefa91f9559bff2cc09
vivinkv6 <vivinkv0@gmail.com>
2023-09-28 16:46:01 +0530

    update

--- App.jsx
+++ App.jsx
@@ -114,7 +114,7 @@ export default function App() {
         setEmoji={setEmoji}
       />
 
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </GestureHandlerRootView>
   );
 }
